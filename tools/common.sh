f886ec0de59684d1e3538d098ded554095dfe6b0
RYAN A RIVERA <rrivera@FNAL.GOV>
2024-02-28 13:01:45 -0600

    Improved bash script echo output handling to route through TRACE

--- tools/common.sh
+++ tools/common.sh
@@ -1,85 +1,86 @@
+#!/bin/sh
 # Source this to get color code variables to use during output
 # Not sure why this is encapsulated into a function...
 
 defineColors ()
 {
   # Regular Colors
-  Black='\033[0;30m'        # Black
-  Red='\033[0;31m'          # Red
-  Green='\033[0;32m'        # Green
-  Yellow='\033[0;32m'       # \033[0;33m  Yellow -- too hard to see on white (so making green)
-  Blue='\033[0;34m'         # Blue
-  Purple='\033[0;35m'       # Purple
-  Cyan='\033[0;36m'         # Cyan
-  White='\033[0;37m'        # White
+  Black=`printf '\033[0;30m'`        # Black
+  Red=`printf '\033[0;31m'`          # Red
+  Green=`printf '\033[0;32m'`        # Green
+  Yellow=`printf '\033[0;32m'`       # \033[0;33m  Yellow -- too hard to see on white (so making green)
+  Blue=`printf '\033[0;34m'`         # Blue
+  Purple=`printf '\033[0;35m'`       # Purple
+  Cyan=`printf '\033[0;36m'`         # Cyan
+  White=`printf '\033[0;37m'`        # White
 
   # Bold
-  BBlack='\033[1;30m'       # Black
-  BRed='\033[1;31m'         # Red
-  BGreen='\033[1;32m'       # Green
-  BYellow='\033[1;33m'      # Yellow
-  BBlue='\033[1;34m'        # Blue
-  BPurple='\033[1;35m'      # Purple
-  BCyan='\033[1;36m'        # Cyan
-  BWhite='\033[1;37m'       # White
+  BBlack=`printf '\033[1;30m'`       # Black
+  BRed=`printf '\033[1;31m'`         # Red
+  BGreen=`printf '\033[1;32m'`       # Green
+  BYellow=`printf '\033[1;33m'`      # Yellow
+  BBlue=`printf '\033[1;34m'`        # Blue
+  BPurple=`printf '\033[1;35m'`      # Purple
+  BCyan=`printf '\033[1;36m'`        # Cyan
+  BWhite=`printf '\033[1;37m'`       # White
 
   # Underline
-  UBlack='\033[4;30m'       # Black
-  URed='\033[4;31m'         # Red
-  UGreen='\033[4;32m'       # Green
-  UYellow='\033[4;33m'      # Yellow
-  UBlue='\033[4;34m'        # Blue
-  UPurple='\033[4;35m'      # Purple
-  UCyan='\033[4;36m'        # Cyan
-  UWhite='\033[4;37m'       # White
+  UBlack=`printf '\033[4;30m'`       # Black
+  URed=`printf '\033[4;31m'`         # Red
+  UGreen=`printf '\033[4;32m'`       # Green
+  UYellow=`printf '\033[4;33m'`      # Yellow
+  UBlue=`printf '\033[4;34m'`        # Blue
+  UPurple=`printf '\033[4;35m'`      # Purple
+  UCyan=`printf '\033[4;36m'`        # Cyan
+  UWhite=`printf '\033[4;37m'`       # White
 
   # Background
-  On_Black='\033[40m'       # Black
-  On_Red='\033[41m'         # Red
-  On_Green='\033[42m'       # Green
-  On_Yellow='\033[43m'      # Yellow
-  On_Blue='\033[44m'        # Blue
-  On_Purple='\033[45m'      # Purple
-  On_Cyan='\033[46m'        # Cyan
-  On_White='\033[47m'       # White
+  On_Black=`printf '\033[40m'`       # Black
+  On_Red=`printf '\033[41m'`         # Red
+  On_Green=`printf '\033[42m'`       # Green
+  On_Yellow=`printf '\033[43m'`      # Yellow
+  On_Blue=`printf '\033[44m'`        # Blue
+  On_Purple=`printf '\033[45m'`      # Purple
+  On_Cyan=`printf '\033[46m'`        # Cyan
+  On_White=`printf '\033[47m'`       # White
 
   # High Intensity
-  IBlack='\033[0;90m'       # Black
-  IRed='\033[0;91m'         # Red
-  IGreen='\033[0;92m'       # Green
-  IYellow='\033[0;31m'      #'\033[0;93m'      # Yellow  -- too hard to see on white (so making light red)
-  IBlue='\033[0;94m'        # Blue
-  IPurple='\033[0;95m'      # Purple
-  ICyan='\033[0;96m'        # Cyan
-  IWhite='\033[0;97m'       # White
+  IBlack=`printf '\033[0;90m'`       # Black
+  IRed=`printf '\033[0;91m'`         # Red
+  IGreen=`printf '\033[0;92m'`       # Green
+  IYellow=`printf '\033[0;31m'`      #'\033[0;93m'      # Yellow  -- too hard to see on white (so making light red)
+  IBlue=`printf '\033[0;94m'`        # Blue
+  IPurple=`printf '\033[0;95m'`      # Purple
+  ICyan=`printf '\033[0;96m'`        # Cyan
+  IWhite=`printf '\033[0;97m'`       # White
 
   # Bold High Intensity
-  BIBlack='\033[1;90m'      # Black
-  BIRed='\033[1;91m'        # Red
-  BIGreen='\033[1;92m'      # Green
-  BIYellow='\033[1;93m'     # Yellow
-  BIBlue='\033[1;94m'       # Blue
-  BIPurple='\033[1;95m'     # Purple
-  BICyan='\033[1;96m'       # Cyan
-  BIWhite='\033[1;97m'      # White
+  BIBlack=`printf '\033[1;90m'`      # Black
+  BIRed=`printf '\033[1;91m'`        # Red
+  BIGreen=`printf '\033[1;92m'`      # Green
+  BIYellow=`printf '\033[1;93m'`     # Yellow
+  BIBlue=`printf '\033[1;94m'`       # Blue
+  BIPurple=`printf '\033[1;95m'`     # Purple
+  BICyan=`printf '\033[1;96m'`       # Cyan
+  BIWhite=`printf '\033[1;97m'`      # White
 
   # High Intensity backgrounds
-  On_IBlack='\033[0;100m'   # Black
-  On_IRed='\033[0;101m'     # Red
-  On_IGreen='\033[0;102m'   # Green
-  On_IYellow='\033[0;103m'  # Yellow
-  On_IBlue='\033[0;104m'    # Blue
-  On_IPurple='\033[0;105m'  # Purple
-  On_ICyan='\033[0;106m'    # Cyan
-  On_IWhite='\033[0;107m'   # White
+  On_IBlack=`printf '\033[0;100m'`   # Black
+  On_IRed=`printf '\033[0;101m'`     # Red
+  On_IGreen=`printf '\033[0;102m'`   # Green
+  On_IYellow=`printf '\033[0;103m'`  # Yellow
+  On_IBlue=`printf '\033[0;104m'`    # Blue
+  On_IPurple=`printf '\033[0;105m'`  # Purple
+  On_ICyan=`printf '\033[0;106m'`    # Cyan
+  On_IWhite=`printf '\033[0;107m'`   # White
 
+  RstClr=`printf '\033[0m'`         # Reset color
   Bold=`tput bold -T xterm`          # Select bold mode                  
   DIM=`tput dim -T xterm`            # Select dim (half-bright) mode     
   Blink=`tput blink -T xterm`        # Select dim (half-bright) mode 
   EUNDERLINE=`tput smul -T xterm`    # Enable underline mode             
   DUNDERLINE=`tput rmul -T xterm`    # Disable underline mode            
   REV=`tput rev -T xterm`            # Turn on reverse video mode        
-  RstClr='\e[0m'         # Reset color
 #Reset=`tput init -T xterm`         # Reset all
   Reset=`tput init -T xterm 2>/dev/null;tput sgr0 -T xterm`         # Reset all
   EBold=`tput smso -T xterm`         # Enter standout (bold) mode        
@@ -88,11 +89,15 @@ defineColors ()
 
 defineColors
 
-SCRIPT_NAME=$1
-out() { echo -e "${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}$(date +'%d%h%y.%T') ${IBlue}${THIS_HOST}${RstClr} ${SCRIPT_NAME}:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}\t${RstClr}$@${RstClr}"; }
-info() { echo -e "${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}$(date +'%d%h%y.%T') ${IBlue}${THIS_HOST}${RstClr} ${SCRIPT_NAME}:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}\t${RstClr}${IBlue}$@${RstClr}"; }
-success() { echo -e "${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}$(date +'%d%h%y.%T') ${IBlue}${THIS_HOST}${RstClr} ${SCRIPT_NAME}:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}\t${RstClr}${IGreen}$@${RstClr}"; }
-error() { echo -e "${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}$(date +'%d%h%y.%T') ${IBlue}${THIS_HOST}${RstClr} ${SCRIPT_NAME}:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}\t${RstClr}${IRed}$@${RstClr}"; } >&2
-warning() { echo -e "${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}$(date +'%d%h%y.%T') ${IBlue}${THIS_HOST}${RstClr} ${SCRIPT_NAME}:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}\t${RstClr}${IYellow}$@${RstClr}"; } >&2
-die() { echo -e "${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}$(date +'%d%h%y.%T') ${IBlue}${THIS_HOST}${RstClr} ${SCRIPT_NAME}:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}\t${RstClr}${IRed}$@${RstClr}"; exit 1; }
+#SCRIPT_NAME=$1
+out()     { pp="${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}%T ${IBlue}${THIS_HOST}${RstClr} %n:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}	${RstClr}%M${RstClr}";           do_out TLVL_LOG     "$*"; }
+info()    { pp="${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}%T ${IBlue}${THIS_HOST}${RstClr} %n:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}	${RstClr}${IBlue}%M${RstClr}";   do_out TLVL_INFO    "$*"; }
+success() { pp="${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}%T ${IBlue}${THIS_HOST}${RstClr} %n:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}	${RstClr}${IGreen}%M${RstClr}";  do_out TLVL_NOTICE  "$*"; }
+warning() { pp="${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}%T ${IBlue}${THIS_HOST}${RstClr} %n:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}	${RstClr}${IYellow}%M${RstClr}"; do_out TVLV_WARNING "$*"; } >&2
+error()   { pp="${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}%T ${IBlue}${THIS_HOST}${RstClr} %n:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}	${RstClr}${IRed}%M${RstClr}";    do_out TLVL_ERROR   "$*"; } >&2
+die()     { pp="${RstClr}${IRed}${STARTTIME}${RstClr}-${Green}%T ${IBlue}${THIS_HOST}${RstClr} %n:${Cyan}${BASH_LINENO[0]}${RstClr} |${IBlack}	${RstClr}${IRed}%M${RstClr}";    do_out TLVL_FATAL   "$*"; exit 1; }
 
+do_out() { tlvl=$1;shift;TRACE_TIME_FMT=%d%h%y.%T TRACE_PRINT="$pp" trace_cntl -n`basename ${BASH_SOURCE[2]}` -L${BASH_LINENO[1]} TRACE TLVL_LOG "$(echo -e "$*")"; }
+
+#out  common2.sh done
+#out2 common2.sh done
