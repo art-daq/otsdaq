356d88a88a704cae7683686eccd251fe8e4a527f
Eric Flumerfelt <eflumerf@fnal.gov>
2024-03-07 18:44:57 -0600

    Fix overrides, add new s-qualifiers, and update version number

--- otsdaq/ConfigurationInterface/Database_configInterface.h
+++ otsdaq/ConfigurationInterface/Database_configInterface.h
@@ -25,29 +25,29 @@ class DatabaseConfigurationInterface : public ConfigurationInterface
 	bool 									IS_FILESYSTEM_DB = true;
 
 	// read table from database
-	void 									fill						(TableBase* table, TableVersion version) const;
+	void 									fill						(TableBase* table, TableVersion version) const override;
 
 	// write table to database	
-	void 									saveActiveVersion			(const TableBase* table, bool overwrite = false) const;
+	void 									saveActiveVersion			(const TableBase* table, bool overwrite = false) const override;
 
 	// find the latest table version by table type	
-	TableVersion 							findLatestVersion			(const TableBase* table) const noexcept;
+	TableVersion 							findLatestVersion			(const TableBase* table) const noexcept override;
 
 	// returns a list of all table names	
-	std::set<std::string /*name*/> 			getAllTableNames			(void) const;
+	std::set<std::string /*name*/> 			getAllTableNames			(void) const override;
 	// find all table versions by table type	
-	std::set<TableVersion> 					getVersions					(const TableBase* table) const noexcept;
+	std::set<TableVersion> 					getVersions					(const TableBase* table) const noexcept override;
 
 	// find all table groups in database	
-	std::set<std::string /*name+version*/> 	getAllTableGroupNames		(std::string const& filterString = "") const;
-	std::set<TableGroupKey>                	getKeys						(const std::string& groupName) const;
-	TableGroupKey                          	findLatestGroupKey			(const std::string& groupName) const noexcept;
+	std::set<std::string /*name+version*/> 	getAllTableGroupNames		(std::string const& filterString = "") const override;
+	std::set<TableGroupKey>                	getKeys						(const std::string& groupName) const override;
+	TableGroupKey                          	findLatestGroupKey			(const std::string& groupName) const noexcept override;
 
 	// return the contents of a table group	
-	table_version_map_t 					getTableGroupMembers		(std::string const& tableGroup, bool includeMetaDataTable = false) const;
+	table_version_map_t 					getTableGroupMembers		(std::string const& tableGroup, bool includeMetaDataTable = false) const override;
 
 	// create a new table group from the contents map	
-	void 									saveTableGroup				(table_version_map_t const& memberMap, std::string const& tableGroup) const;
+	void 									saveTableGroup				(table_version_map_t const& memberMap, std::string const& tableGroup) const override;
 
 
 	std::pair<std::string, TableVersion>	saveCustomJSON				(const std::string& JSON, const std::string& documentNameToSave) const override;
