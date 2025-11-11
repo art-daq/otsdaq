4f6c47fbf0efa95e85c3eabea55b3a520bf72de6
Simon Corrodi <scorrodi@anl.gov>
2025-11-11 13:26:54 -0600

    TCPDataListener fix when re-running (work in progress)

--- otsdaq/NetworkUtilities/TCPServerBase.h
+++ otsdaq/NetworkUtilities/TCPServerBase.h
@@ -20,6 +20,7 @@ class TCPServerBase : public virtual TCPSocket
 	virtual ~TCPServerBase(void);
 
 	void startAccept(void);
+    void shutdownAccept(void);
 	void broadcastPacket(const char* message, std::size_t length);
 	void broadcastPacket(const std::string& message);
 	void broadcast(const char* message, std::size_t length);
@@ -51,7 +52,6 @@ class TCPServerBase : public virtual TCPSocket
 	void closeClientSockets(
 	    void);  ///< This one will also wait until the socket thread is done!
 	int  accept(bool blocking = true);
-	void shutdownAccept(void);
 
 	const int        fMaxConnectionBacklog = 5;
 	unsigned int     fMaxNumberOfClients;
