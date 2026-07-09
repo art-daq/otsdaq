eba5a57255cfe92675b31970d9b6a0602ed9c98d
Javier Campos <jcampos@fnal.gov>
2026-07-08 21:32:07 -0500

    bugfix of config_dump in the presence of multiple remote configuration/start iterations

--- otsdaq/GatewaySupervisor/GatewaySupervisor.cc
+++ otsdaq/GatewaySupervisor/GatewaySupervisor.cc
@@ -9995,7 +9995,8 @@ void GatewaySupervisor::broadcastMessageToRemoteGateways(
 
 		commandedApps.emplace(remoteGatewayApp.fullName);
 
-		remoteGatewayApp.config_dump = "";  //clear, must come from new command completion
+		if(iteration == 0)
+			remoteGatewayApp.config_dump = "";  //clear on first iteration only; subsequent iterations preserve the dump already received
 		remoteGatewayApp.command     = commandAndParams;
 
 		remoteGatewayApp.command +=
@@ -10045,7 +10046,8 @@ void GatewaySupervisor::broadcastMessageToRemoteGateways(
 					{
 						rga.command          = localApp.command;
 						rga.fsmName          = localApp.fsmName;
-						rga.config_dump      = localApp.config_dump;
+						if(localApp.config_dump.size())
+							rga.config_dump      = localApp.config_dump;
 						rga.appInfo.status   = localApp.appInfo.status;
 						rga.appInfo.progress = localApp.appInfo.progress;
 					}
