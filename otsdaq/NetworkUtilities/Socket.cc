8ad960c72f4a8275223096a0dbe54cb8dc7f3a07
Simon Corrodi <scorrodi@anl.gov>
2026-09-25 00:07:21 -0500

    Socket: close socket when bind fails in port search
    The leaked file descriptor caused ots to crash when many sockets were
    opened and closed during configuration.

--- otsdaq/NetworkUtilities/Socket.cc
+++ otsdaq/NetworkUtilities/Socket.cc
@@ -129,6 +129,7 @@ void Socket::initialize(unsigned int socketReceiveBufferSize)
 		{
 			__COUTT__ << "]\tPort " << port.str() << " unavailable, trying next port..."
 			          << std::endl;
+			close(socketNumber_);  // otherwise every occupied port leaks a descriptor
 			socketNumber_ = 0;
 		}
 		else
